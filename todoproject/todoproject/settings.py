0af8608e7551735b750dfbc7f2b95331810548c0
RonAlffrson <renan.c.alves@edu.unirio.br>
2024-03-19 17:39:26 -0300

    debug false

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#uvwoyy+7k18$-bdt(lm*a8cy0vs7#+szes2y(t7o-(fup_m1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
